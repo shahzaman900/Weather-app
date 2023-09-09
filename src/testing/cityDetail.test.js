9035ac41648108a8d46c51b137a972cc8daedea8
sheezu900 <shahzamanlive2@gmail.com>
2023-09-10 01:39:06 +0500

    .

--- src/testing/cityDetail.test.js
+++ src/testing/cityDetail.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from '../redux/store';
+import store from '../Redux/store';
 import CityDetail from '../component/cityDetail';
 
 test('Check if Missions component is rendered correctly', () => {

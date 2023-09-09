0363cc78e2db18a78d381a6dc1630a9ae0f92b9a
sheezu900 <shahzamanlive2@gmail.com>
2023-09-10 02:34:32 +0500

    add integration test

--- src/testing/cityDetail.test.js
+++ src/testing/cityDetail.test.js
@@ -1,17 +1,20 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router-dom';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import store from '../Redux/store';
 import CityDetail from '../component/cityDetail';
 
-test('Check if Missions component is rendered correctly', () => {
-  const tree = render(
-    <Provider store={store}>
-      <BrowserRouter>
-        <CityDetail />
-      </BrowserRouter>
-    </Provider>,
-  );
-  expect(tree).toMatchSnapshot();
+describe('testing country detail', () => {
+  it('should render correct country name ', async () => {
+    render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <CityDetail />
+        </BrowserRouter>
+      </Provider>,
+    );
+    const countryCard = screen.findByText('weather');
+    expect(countryCard).toMatchSnapshot();
+  });
 });
